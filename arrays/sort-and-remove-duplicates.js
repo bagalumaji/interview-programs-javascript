df60846979cc88405e0728b3c1c808ffe0904ff4
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-05 10:59:36 +0530

    updated with remove duplicates

--- arrays/sort-and-remove-duplicates.js
+++ arrays/sort-and-remove-duplicates.js
@@ -1,5 +1,10 @@
-import {sortNumbers} from "./sort-numbers";
+import {sortNumbers} from './sort-numbers.js';
+import {removeDuplicates} from "./remove-duplicates.js";
 
 let arr=[2,1,3,2,3,1,5,4,6,3]
 arr = sortNumbers(arr);
 console.log(arr);
+let newLength = removeDuplicates(arr);
+for(let i=0;i<newLength;i++){
+    console.log(arr[i]);
+}
