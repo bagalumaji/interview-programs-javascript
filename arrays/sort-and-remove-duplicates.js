cd25e51f3d1b04705a7243bc2881e93539258f30
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-05 11:11:06 +0530

    updated method

--- arrays/sort-and-remove-duplicates.js
+++ arrays/sort-and-remove-duplicates.js
@@ -1,10 +1,26 @@
-import {sortNumbers} from './sort-numbers.js';
-import {removeDuplicates} from "./remove-duplicates.js";
+import {sortNumbers, sortNumbers2} from './sort-numbers.js';
+import {removeDuplicates, removeDuplicates2} from "./remove-duplicates.js";
 
 let arr=[2,1,3,2,3,1,5,4,6,3]
 arr = sortNumbers(arr);
+console.log( "after sorting")
 console.log(arr);
+
+console.log("after removing duplicates")
 let newLength = removeDuplicates(arr);
 for(let i=0;i<newLength;i++){
     console.log(arr[i]);
 }
+
+
+
+//second approach
+let arr2=[2,1,3,2,3,1,5,4,6,3]
+
+let a = removeDuplicates2(arr2)
+console.log("after removing duplicates")
+console.log(a);
+
+console.log( "after sorting")
+sortNumbers2(a)
+console.log(a)
