7b12935377b1339c4849d9b4e9b1f91353eda9b6
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-08 19:36:58 +0530

    demo

---
+++ demo.js
@@ -0,0 +1,5 @@
+import {reverseString} from "./strings/reverse-string.js";
+import {reverseStringWithoutInbuildFunction} from "./strings/reverse-string-1.js";
+const s = "Hello World";
+console.log(reverseString(s));
+console.log(reverseStringWithoutInbuildFunction(s));
