b3e9b8d40d7524abe9dfbee9bbef87468bef0413
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-16 16:33:55 +0530

    updated longest substring

--- strings/longest-substring.js
+++ strings/longest-substring.js
@@ -1,26 +1,19 @@
 function longestSubString(str){
-    const set = new Set();
-    let start = 0;
+    let start=0;
+    // let max=0;
     let s="";
-    let end =0;
-    let max=0;
-    while(end<str.length){
-            if(!set.has(str.charAt(end))){
-                set.add(str.charAt(end));
-                end++;
-            }else{
-                set.delete(str.charAt(start));
-                start++;
-            }
-           if(max<(end-start)){
-            max = end-start
-            s = str.substring(start,end);
-           }
-           
-    } 
-    console.log(str)
-    console.log(max);
-    console.log(s);
-
+    const set = new Set();
+    for(let end=0;end<str.length;end++){
+        while(set.has(str.charAt(end))){
+            set.delete(str.charAt(start));
+            start++;
+        }
+        set.add(str.charAt(end));
+        s = str.substring(start,end+1);
+        // max = Math.max(max,s.length);
+    }
+    return s;
 }
-longestSubString("abcabcdefs");
+const str = longestSubString("abcabcdefs")
+console.log("longest substring is : "+str);
+console.log("length of longest substring is : "+str.length);
